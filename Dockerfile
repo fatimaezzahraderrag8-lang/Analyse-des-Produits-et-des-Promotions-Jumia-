2ca760684d34241537959261df2a440465cd919d
fatimaezzahra <fatimaezzahraderrag8@gmail.com>
2026-08-03 15:36:07 +0100

    ajouter presentation et modifier codes

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,14 @@ FROM apache/airflow:2.10.0-python3.11
 
 USER root
 
-RUN apt-get update && apt-get install -y gcc libpq-dev
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    gcc \
+    libpq-dev \
+    && apt-get clean
 
 USER airflow
 
-COPY requirements.txt .
+COPY requirements.txt /requirements.txt
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r /requirements.txt
--- etl/load_staging.py
+++ etl/load_staging.py
@@ -6,11 +6,13 @@ from sqlalchemy import create_engine, text
 
 load_dotenv()
 
+
 def load_staging(file_path):
 
     if not os.path.exists(file_path):
-        raise FileNotFoundError(file_path)
+        raise FileNotFoundError(f"Fichier introuvable : {file_path}")
 
+    # Lecture du fichier CSV
     df = pd.read_csv(file_path)
 
     user = os.getenv("DB_USER")
@@ -19,20 +21,22 @@ def load_staging(file_path):
     port = os.getenv("DB_PORT")
     db = os.getenv("DB_NAME")
 
+    # Connexion à PostgreSQL
     conn = psycopg2.connect(
         host=host,
         port=port,
         user=user,
         password=password,
-        dbname="postgres"
+        dbname="postgres",
     )
 
     conn.autocommit = True
     cursor = conn.cursor()
 
+    # Création de la base si elle n'existe pas
     cursor.execute(
         "SELECT 1 FROM pg_database WHERE datname=%s",
-        (db,)
+        (db,),
     )
 
     if cursor.fetchone() is None:
@@ -47,19 +51,20 @@ def load_staging(file_path):
 
     engine = create_engine(DATABASE_URL)
 
+    # Création du schéma staging
     with engine.begin() as connection:
         connection.execute(text("CREATE SCHEMA IF NOT EXISTS staging"))
 
+    # Chargement des données
     df.to_sql(
-        "products_raw",
+        name="products_raw",
         con=engine,
         schema="staging",
         if_exists="replace",
         index=False,
-        method="multi"
     )
 
-    print("Chargement staging terminé.")
+    print("Chargement dans staging terminé avec succès.")
 
 
 if __name__ == "__main__":
